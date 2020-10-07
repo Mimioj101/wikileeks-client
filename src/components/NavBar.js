4a3849cfd74109cb7bd0973a3f41c3a33e3958ce
Mimioj101 <mimioj@gmail.com>
2020-10-07 15:37:18 -0400

    started navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,47 @@
+import React from "react"
+import { NavLink } from 'react-router-dom';
+
+
+const link = {
+    width: '100px',
+    padding: '12px',
+    margin: '0 6px 6px',
+    background: 'blue',
+    textDecoration: 'none',
+    color: 'white',
+  }
+
+  class NavBar extends React.Component {
+    render() {
+      return (
+        <div className="navbar">
+          <NavLink
+            to="/"
+            exact
+            style={link}
+            activeStyle={{background: '#6A994E'}}>
+          Home</NavLink>
+          <NavLink
+            to="/bookmarks"
+            exact
+            style={link}
+            activeStyle={{background: '#6A994E'}}>
+          Bookmarks</NavLink>
+          <NavLink
+          to="/logout"
+          exact
+          style={link}
+          activeStyle={{background: '#6A994E'}}>
+          Logout</NavLink> 
+          <NavLink
+            to="/login"
+            exact
+            style={link}
+            activeStyle={{background: '#6A994E'}}>
+          Login</NavLink>
+        </div>
+      );
+    }
+  };
+  
+  export default NavBar;
